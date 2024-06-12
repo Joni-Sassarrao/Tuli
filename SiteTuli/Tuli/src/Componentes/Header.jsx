c5bc8ddbda9e3cb54dc0909762272e1280808c94
Johnny <jp.sdc006@gmail.com>
2024-06-12 13:24:38 -0300

    Tuli

---
+++ SiteTuli/Tuli/src/Componentes/Header.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./Componenete/NavBar"
+
+export default function Header() {
+    return ( 
+        <>
+        <Header className="flex text-white text-3xl p-4 justify-between">
+           <h1></h1> 
+        </Header>
+        <NavBar/>
+        </>
+    );
+}
