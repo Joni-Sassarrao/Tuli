1fd5f72607856becd784dfa19b4c4af3fc4006a3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 10:05:41 -0300

    Tuli

--- SiteTuli/TuliSite/src/Componentes/Header.jsx
+++ SiteTuli/TuliSite/src/Componentes/Header.jsx
@@ -1,9 +1,10 @@
 import NavBar from "./NavBar";
+import Logo from "../../public/Tuli.svg"
 
 function Header() {
     return ( 
-        <header className="flex flex-row bg-black">
-            <h1>tuli</h1>
+        <header className="flex flex-row bg-black p-7 text-white justify-between items-center">
+            <img className="" src={Logo}/>
             <NavBar/>
         </header>
     )
--- SiteTuli/TuliSite/src/Componentes/NavBar.jsx
+++ SiteTuli/TuliSite/src/Componentes/NavBar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 function NavBar() {
     return ( 
         <nav>
-            <ul className='flex flex-row text-black'>
+            <ul className='flex gap-3 text-2xl'>
                 <li><Link to={"/"}>Início</Link></li>
                 <li><Link to={"quemsomos"}>Quem Somos?</Link></li>
                 <li><Link to={"especialista"}>Especialistas</Link></li>
