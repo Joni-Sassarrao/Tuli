4a340ad8add0168a48888b10fc7c01a1503a9a69
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-25 13:39:51 -0300

    Tuli

--- SiteTuli/TuliSite/src/Pages/QuemSomos.jsx
+++ SiteTuli/TuliSite/src/Pages/QuemSomos.jsx
@@ -1,7 +1,10 @@
 export default function QuemSomos() {
     return ( 
-        <>
-        <h1>Quem Somos?</h1>
-        </>
+        <div>
+            <img src={"/flor2.svg"}/>
+            <div className="flex items-end justify-end">
+                <h1 className="absolute items-end right w-[200px] bottom-1 font-josefin m-10 text-white text-6xl">Quem Somos?</h1>
+            </div>
+        </div>
     )
 }
