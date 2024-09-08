e593feaa801c4df005f5725c71291c5c2309946b
Johnny <jp.sdc006@gmail.com>
2024-09-08 01:15:12 -0300

    TCC

--- TCC/Tuli/tailwind.config.js
+++ TCC/Tuli/tailwind.config.js
@@ -12,11 +12,15 @@ export default {
         'prompt': ['Prompt', 'sans-serif']
       },
       colors: {
-        'fundo1': "#D9B1AD",
+        'fundo1': '#D9B1AD',
         'fundo2': '#F2DFDF',
-        'fundo2(o)': "rgba(242, 223, 223, 0.5)",
-        'texto1': "#595859",
-        'texto2': "#A95858",
+        'fundo2(o)': 'rgba(242, 223, 223, 0.5)',
+        'texto1': '#595859',
+        'texto2': '#A95858',
+        'texto3': 'rgba(51, 51, 51, 0.8)',
+      },
+      boxShadow: {
+        'Card': '0 3px 1px rgba(0, 0, 0, 0.12), 0 4px 2px rgba(1, 0, 0, 0.12)',
       }
     },
   },
