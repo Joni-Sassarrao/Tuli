ce1a4786bfb1b7a89dd586378a64b2e148a98365
Johnny <jp.sdc006@gmail.com>
2024-09-18 13:34:43 -0300

    TCC

--- TCC/Tuli/src/Pages/Especialista.jsx
+++ TCC/Tuli/src/Pages/Especialista.jsx
@@ -2,7 +2,14 @@ export default function Especialista(){
     return(
         <>
         <div className="h-[25rem] bg-black">
-
+            <div>
+                <div>
+                    <h1>QUER FAZER PARTE DA NOSSA EQUIPE</h1>
+                </div>
+                <div>
+                    <img src={''}/>
+                </div>
+            </div>
         </div>
         </>
     )
