e8713b778879add2e398868d4e54701f8f4ae9ca
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 11:21:08 -0300

    TCC

--- TCC/Tuli/src/Componentes/Home/Inside-Image-Home.jsx
+++ TCC/Tuli/src/Componentes/Home/Inside-Image-Home.jsx
@@ -3,7 +3,7 @@ export default function InsideImageHome(){
         <>
         <div className="text-texto1 p-14 absolute max-desktop100:mt-2">
             <h2 className="text-4xl font-prompt mt-12 mb-5 max-desktop100:text-3xl max-desktop110:text-2xl">Bem-vindo à Tuli</h2>
-            <h1 className="text-6xl font-nunito font-bold w-[57rem] mb-5 max-desktop90:w-[45rem] max-desktop90:text-5xl max-desktop100:w-[40rem] max-desktop100:text-[2.7rem] max-desktop110:w-[34rem] max-desktop110:text-4xl">A BELEZA QUE VOCÊ DESEJA, COM A SEGURANÇA QUE VOCÊ MERECE.</h1>
+            <h1 className="text-6xl font-nunito font-bold w-[57rem] mb-5 max-desktop90:w-[45rem] max-desktop90:text-5xl max-desktop100:w-[40rem] max-desktop100:text-[2.7rem] max-desktop110:w-[34rem] max-desktop110:text-4xl max-[1000px]:w-[28rem] max-[1000px]:text-3xl">A BELEZA QUE VOCÊ DESEJA, COM A SEGURANÇA QUE VOCÊ MERECE.</h1>
             <p className="text-2xl font-prompt w-[30rem] max-desktop110:w-[25rem] max-desktop110:text-xl">Transforme sua Beleza com Confiança e Praticidade.</p>
         </div>
         <div className="w-full flex justify-end">
