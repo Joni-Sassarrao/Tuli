4d950345f24fd7a37609868f39f08bae9a5842e3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-15 13:22:19 -0300

    TCC

--- TCC/Tuli/src/Componentes/Home/Inside-Container-Home2.jsx
+++ TCC/Tuli/src/Componentes/Home/Inside-Container-Home2.jsx
@@ -1,24 +1,24 @@
 export default function InsideContainerHome2(){
     return(
         <>
-        <div className="w-[350px] h-[450px] rounded-3xl shadow-Card max-desktop5:w-[300px] max-desktop5:h-[250px]">
+        <div className="w-[350px] h-[450px] rounded-3xl shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
             <img className="w-auto max-desktop5:w-0" src={'/toxina.svg'}/>
             <div className="flex flex-col justify-center p-5">
                 <h2 className="text-texto2 text-xl font-prompt mb-5">Toxina Botulínica</h2>
                 <p className="text-texto3 text-lg font-prompt">Reduz rugas e linhas de expressão ao relaxar temporariamente os músculos faciais.</p>
             </div>
         </div>
-        <div className="flex flex-col justify-between ml-10 max-desktop5:flex-row justify-inicial max-desktop5:-ml-20">
-            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+        <div className="flex flex-col justify-between ml-10 max-desktop5:flex-row max-desktop5:-mr-3 max-desktop5:-ml-[9rem]">
+            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <div className="flex justify-start w-full max-desktop5:w-0">
                     <img className="w-auto max-desktop5:w-0" src={'/bichectomia.svg'}/>
                 </div>
                 <div className="p-5">
                     <h2 className="text-texto2 text-xl font-prompt mb-5">Bichectomia</h2>
-                    <p className="text-texto3 text-lg font-prompt">Cirurgia para remoção das bolsas de gordura nasbochechas, visando afinar o rosto.</p>
+                    <p className="text-texto3 text-lg font-prompt">Cirurgia para remoção das bolsas de gordura nas bochechas, visando afinar o rosto.</p>
                 </div>
             </div>
-            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <div className="p-5">
                     <h2 className="text-texto2 text-xl font-prompt mb-5">Preenchimento Labial</h2>
                     <p className="text-texto3 text-lg font-prompt">Aumenta o volume dos lábios usando substâncias como ácido hialurônico.</p>
@@ -28,15 +28,15 @@ export default function InsideContainerHome2(){
                 </div>
             </div>
         </div>
-        <div className="flex flex-col justify-between ml-10 max-desktop5:flex-row">
-            <div className="w-[320px] rounded-[1.4rem] shadow-Card">
-                <img className="w-auto max-desktop5:w-0" src={'peeling.svg'}/>
+        <div className="flex flex-col justify-between ml-10 max-desktop5:flex-row max-desktop5:mt-5 max-desktop5:w-[40rem] max-desktop5:ml-[12rem]">
+            <div className="w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
+                <img className="w-auto max-desktop5:w-0" src={'/peeling.svg'}/>
                 <div className="p-3">
-                    <h2 className="text-texto2 font-promtp text-xl mb-4">Peeling Químico</h2>
+                    <h2 className="text-texto2 font-prompt text-xl mb-4">Peeling Químico</h2>
                     <p className="text-texto3 font-prompt text-lg">Usa ácidos para esfoliar a pele, melhorando a textura e reduzindo imperfeições.</p>
                 </div>
             </div>
-            <div className="flex h-[140px] w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+            <div className="flex h-[140px] w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <div className="flex justify-start w-full max-desktop5:w-0">
                     <img className="w-auto max-desktop5:w-0" src={'/microagulhamento.svg'}/>
                 </div>
--- TCC/Tuli/src/Componentes/Home/Inside-Container-Home3.jsx
+++ TCC/Tuli/src/Componentes/Home/Inside-Container-Home3.jsx
@@ -1,15 +1,15 @@
 export default function InsideContainerHome3(){
     return(
         <>
-        <div className="flex flex-col justify-between mr-10 max-desktop5:flex-row">
-            <div className="w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+        <div className="flex flex-col justify-between mr-10 max-desktop5:ml-[11.7rem]">
+            <div className="w-[320px] h-[300px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <img className="w-auto max-desktop5:w-0" src={'/drenagem.svg'}/>
                 <div className="p-3">
-                    <h2 className="text-texto2 font-prompt text-xl mb-4">Drenagem linfática</h2>
-                    <p>Massagem especializada que estimula o sistema linfático para reduzir inchaços e melhorar a circulação de fluidos corporais.</p>
+                    <h2 className="text-texto2 font-prompt text-xl mb-2">Drenagem linfática</h2>
+                    <p className="text-texto3 font-prompt text-lg">Massagem especializada que estimula o sistema linfático, reduz inchaços e melhora a circulação de fluidos corporais.</p>
                 </div>
             </div>
-            <div className="flex h-[140px] w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+            <div className="flex h-[140px] w-[320px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <div className="flex justify-start w-full max-desktop5:w-0">
                     <img className="w-auto max-desktop5:w-0" src={'/mamoplastia.svg'}/>
                 </div>
@@ -19,8 +19,8 @@ export default function InsideContainerHome3(){
                 </div>
             </div>
         </div>
-        <div className="flex flex-col justify-between mr-10 max-desktop5:justify-inicial">
-            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:h-[300px]">
+        <div className="flex flex-col justify-between mr-10 max-desktop5:ml-[2.5rem]">
+            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px] max-desktop5:mb-7">
                 <div className="flex justify-start w-full max-desktop5:w-0">
                     <img className="w-auto max-desktop5:w-5" src={'/lipospiracao.svg'}/>
                 </div>
@@ -29,7 +29,7 @@ export default function InsideContainerHome3(){
                     <p className="text-texto3 font-prompt text-lg">Em cirurgia, emove excesso de gordura localizada através de sucção.</p>
                 </div>
             </div>
-            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[200px] max-desktop5:h-[250px]">
+            <div className="flex w-[420px] rounded-[1.4rem] shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px]">
                 <div className="p-5">
                     <h2 className="text-texto2 font-prompt text-xl mb-5">Microagulhamento Capilar</h2>
                     <p className="text-texto3 font-prompt text-lg">Estimulação do crescimento capilar por meio de agulhas pequenas.</p>
@@ -39,7 +39,7 @@ export default function InsideContainerHome3(){
                 </div>
             </div>
         </div>
-        <div className="w-[350px] h-[450px] rounded-3xl shadow-Card max-desktop5:w-[300px] max-desktop5:h-[250px] max-desktop5:absolute">
+        <div className="w-[350px] h-[460px] rounded-3xl shadow-Card max-desktop5:w-[300px] max-desktop5:h-[200px] max-desktop5:ml-[22rem] max-desktop5:mt-10">
             <img className="w-auto max-desktop5:w-0" src={'/limpeza.svg'}/>
             <div className="flex flex-col justify-center p-5">
                 <h2 className="text-texto2 font-prompt text-xl mb-5">Limpeza de Pele </h2>
