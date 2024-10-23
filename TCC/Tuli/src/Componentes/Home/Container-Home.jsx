c9dc2fa07dc63f679004d2893a53ba113da6d4cf
Letacio-Bezerra <henryyuji.tr@gmail.com>
2024-10-23 16:34:03 -0300

    TCC

--- TCC/Tuli/src/Componentes/Home/Container-Home.jsx
+++ TCC/Tuli/src/Componentes/Home/Container-Home.jsx
@@ -1,7 +1,7 @@
 export default function ContainerHome({children}){
     return(
         <>
-        <div className="flex justify-evenly bg-fundo2(o) h-[260px] p-5">
+        <div className="flex justify-evenly bg-fundo2(o) h-[260px] p-5 max-desktop6.1:gap-3">
             {children}
         </div>
         </>
--- TCC/Tuli/src/Componentes/Home/Inside-Container-Home.jsx
+++ TCC/Tuli/src/Componentes/Home/Inside-Container-Home.jsx
@@ -2,31 +2,31 @@ export default function InsideContainerHome(){
     return(
         <>
         <div className="mt-5">
-            <div className="bg-fundo2 h-32 w-32 rounded-full absolute"></div>
-            <img className="relative ml-16" src={"/confiabilidade.svg"}/>
+            <div className="bg-fundo2 h-32 w-32 rounded-full absolute max-desktop8:h-24 max-desktop8:w-24"></div>
+            <img className="relative ml-16 max-desktop6.1:w-24" src={"/confiabilidade.svg"}/>
             <div className="flex flex-col mt-4">
-                <p className="font-prompt text-texto2 -ml-1 text-2xl">Confiabilidade</p>
+                <p className="font-prompt text-texto2 -ml-1 text-2xl max-desktop6.1:text-xl">Confiabilidade</p>
             </div>
         </div>
         <div className="mt-5">
-            <div className="bg-fundo2 h-32 w-32 rounded-full absolute"></div>
-            <img className="relative ml-20" src={"/consultas.svg"}/>
+            <div className="bg-fundo2 h-32 w-32 rounded-full absolute max-desktop8:h-24 max-desktop8:w-24"></div>
+            <img className="relative ml-20 max-desktop6.1:w-[5.5rem]" src={"/consultas.svg"}/>
             <div className="flex flex-col mt-4">
-                <p className="font-prompt text-texto2 -ml-5 text-2xl">Agende Consultas</p>
+                <p className="font-prompt text-texto2 -ml-5 text-2xl max-desktop6.1:text-center max-desktop6.1:text-xl">Agende Consultas</p>
             </div>
         </div>
         <div className="mt-5">
-            <div className="bg-fundo2 h-32 w-32 rounded-full absolute"></div>
-            <img className="relative mt-8 ml-20" src={"/profissionais.svg"}/>
+            <div className="bg-fundo2 h-32 w-32 rounded-full absolute max-desktop8:h-24 max-desktop8:w-24"></div>
+            <img className="relative mt-8 ml-20 max-desktop6.1:w-24" src={"/profissionais.svg"}/>
             <div className="flex flex-col mt-4">
-                <p className="font-prompt text-texto2 -ml-5 text-2xl max-desktop4:w-[12rem] max-desktop4:text-center max-desktop5:w-[12rem] max-desktop5:text-center">Melhores Profissionais</p>
+                <p className="font-prompt text-texto2 -ml-5 text-2xl max-desktop4:w-[12rem] max-desktop4:text-center max-desktop5:w-[12rem] max-desktop5:text-center max-desktop6.1:text-xl">Melhores Profissionais</p>
             </div>
         </div>
         <div className="mt-5">
-            <div className="bg-fundo2 h-32 w-32 rounded-full absolute"></div>
-            <img className="relative mt-3 ml-24" src={"/servicos.svg"}/>
+            <div className="bg-fundo2 h-32 w-32 rounded-full absolute max-desktop8:h-24 max-desktop8:w-24"></div>
+            <img className="relative mt-3 ml-24 max-desktop6.1:w-20" src={"/servicos.svg"}/>
             <div className="flex flex-col mt-5">
-                <p className="font-prompt text-texto2 text-center -ml-20 text-2xl w-80">Diversas Opções de Serviços</p>
+                <p className="font-prompt text-texto2 text-center -ml-20 text-2xl w-80 max-desktop6.1:text-xl max-desktop6.1:w-[17rem] max-desktop6.1:text-center">Diversas Opções de Serviços</p>
             </div>
         </div>
         </>
